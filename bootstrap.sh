c7850aa24a3df50763223b70e568f1d46780b6f5
trevoro <trevoro@getebb.com>
2013-10-28 18:30:26 -0700

    removing superfluous sudo from bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
+# all the commands here run as the root user
+
+export DEBIAN_FRONTEND=noninteractive
+
 # setup docker
-sudo apt-get update
-sudo apt-get -y install linux-image-generic-lts-raring linux-headers-generic-lts-raring
+apt-get update
+apt-get -y install linux-image-generic-lts-raring linux-headers-generic-lts-raring
 
 # Add the Docker repository key to your local keychain
-sudo wget -qO- https://get.docker.io/gpg | apt-key add -
+wget -qO- https://get.docker.io/gpg | apt-key add -
 
 # Add the Docker repository to your apt sources list.
-sudo echo 'deb http://get.docker.io/ubuntu docker main' > /etc/apt/sources.list.d/docker.list
+echo 'deb http://get.docker.io/ubuntu docker main' > /etc/apt/sources.list.d/docker.list
 
 # Install, you will see another warning that the package cannot be authenticated.
-sudo apt-get update
-sudo apt-get -y install lxc-docker
+apt-get update
+apt-get -y install lxc-docker
 
-sudo docker pull ubuntu:12.04
+docker pull ubuntu:12.04
 
-sudo shutdown -r now
+shutdown -r now
